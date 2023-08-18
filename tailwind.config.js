0f1c96c4f872299ff39889df14046eb8991d9a8d
proguy18 <josephjoey.leonardi@gmail.com>
2023-08-18 12:40:21 +0700

    chore - init theme colours

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    purge: [],
+    darkMode: false,
+    theme: {
+      extend: {
+        colors: {
+          primary: '#FFFFFF',
+          onPrimary: 'rgba(100, 100, 100, 0.5)',
+          background: '#0F1322',
+          secondary: '#10132A',
+          tertiary: '#11142D',
+          error: '#FF0000'
+        }
+      },
+      fontFamily: {
+        'montserrat': ['Montserrat', 'sans-serif'],
+      }
+    },
+    variants: {},
+    plugins: [],
+  }
+  
