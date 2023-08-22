bc95b2bd34ec17cb294acc0c9d189a8d5647e76c
proguy18 <josephjoey.leonardi@gmail.com>
2023-08-22 21:46:50 +0700

    chore - changed animation from left to right

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,20 +1,36 @@
 <template>
-  <div class="bg-gray-900 text-gray-100 py-3.5 px-6 shadow md:flex justify-between items-center">
-    <div class="flex items-center cursor-pointer">
-      <span class="mr-1 text-green-500 text-x1">
-      </span>
+  <div class="bg-gray-900 text-gray-100 py-3.5 px-6 shadow">
+    <!-- Desktop View -->
+    <div class="hidden md:flex md:flex-col md:items-center md:justify-center">
+      <!-- First Row: Home Button -->
+      <HomeButton class="mb-4">JVLN</HomeButton>
+      <!-- Second Row: Links -->
+      <ul class="flex space-x-4">
+        <li v-for="(link, index) in Links" :key="index">
+          <a :href="link.link" class="text-xl hover:text-green-500">{{ link.name }}</a>
+        </li>
+      </ul>
     </div>
-    <span @click="MenuOpen()" class="absolute md:hidden right-6 top-1.5 cursor-pointer text-4xl">
-      <i :class="[open ? 'bi bi-x' : 'bi bi-filter-left']"></i>
-    </span>
-    <ul
-      class="absolute w-full px-10 pb-10 duration-700 ease-linear bg-gray-900 md:flex md:items-center md:px-0 md:pb-0 md:static md:w-auto top-14"
-      :class="[open ? 'left-0' : 'left-[-100%]']">
-      <li class="my-6 md:mx-4 md:my-0" v-for="(link, index) in Links" :key="index">
-        <a :href="link.link" class="text-xl hover:text-green-500">{{ link.name }}</a>
-      </li>
+
+    <!-- Mobile View -->
+    <div class="flex items-center justify-between md:hidden">
+      <!-- Spacer for left side to balance the menu icon on the right -->
+      <div class="w-10 text-4xl"></div>
+      <!-- Home Button -->
       <HomeButton>JVLN</HomeButton>
-    </ul>
+      <!-- Menu Toggle Icon -->
+      <span @click="MenuOpen()" class="text-4xl cursor-pointer">
+        <i :class="[open ? 'bi bi-x' : 'bi bi-list']"></i>
+      </span>
+      <!-- Dropdown Menu for Links -->
+      <ul class="absolute w-full px-10 pb-10 duration-200 ease-linear bg-gray-900 top-14"
+        :class="[open ? 'right-0' : 'right-[-100%]']">
+        <li class="my-6" v-for="(link, index) in Links" :key="index">
+          <a :href="link.link" class="text-xl hover:text-green-500">{{ link.name }}</a>
+        </li>
+      </ul>
+    </div>
+
   </div>
 </template>
 
@@ -23,7 +39,7 @@ import { ref } from 'vue'
 import HomeButton from './HomeButton.vue'
 
 export default {
-  name: 'NavbarComponent', // Multi-word name
+  name: 'NavbarComponent',
   components: {
     HomeButton,
   },
@@ -37,7 +53,7 @@ export default {
     ]
 
     function MenuOpen() {
-      open.value = !open.value;
+      open.value = !open.value
     }
 
     return {
