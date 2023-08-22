79f73d983d0e9f7d0a9ff5384c3a44a8632b9126
proguy18 <josephjoey.leonardi@gmail.com>
2023-08-22 21:59:45 +0700

    chore - responsive homebutton

--- components/HomeButton.vue
+++ components/HomeButton.vue
@@ -1,9 +1,5 @@
 <template>
-  <button class="text-xl font-thin text-black font-rounded">
+  <button class="w-1/3 py-4 text-5xl font-thin text-black font-rounded md:text-6xl md:w-1/10">
     <slot></slot>
   </button>
 </template>
-
-<script>
-export default {}
-</script>
