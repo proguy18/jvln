ae51acbd3da4d1c556ff22988687b6283ff8c8d2
proguy18 <josephjoey.leonardi@gmail.com>
2023-08-30 01:19:47 +0700

    feat - added fonts

--- components/ContentComponent.vue
+++ components/ContentComponent.vue
@@ -1,25 +1,31 @@
 <template>
-    <div class="p-4 text-center">
-        <h1 class="mb-4 text-xl text-primary">{{ heading }}</h1>
-        <p class="mb-4 text-primary">{{ description }}</p>
-        <a :href="ctaLink" class="px-4 py-2 bg-transparent border rounded border-primary text-primary hover:bg-onPrimary">
-            {{ ctaText }}
-        </a>
+  <div class="p-4 text-left">
+    <!-- Heading with M PLUS Rounded 1c font -->
+    <h1 class="mb-4 text-4xl font-mplus1p text-primary">{{ heading }}</h1>
 
-    </div>
+    <!-- Description with Lato font -->
+    <p class="mb-4 font-lato text-primary">{{ description }}</p>
+
+    <!-- CTA with Lato font -->
+    <a
+      :href="ctaLink"
+      class="px-4 py-2 bg-transparent border rounded font-lato border-primary text-primary hover:bg-onPrimary"
+    >
+      {{ ctaText }}
+    </a>
+  </div>
 </template>
-  
+
 <script>
 export default {
-    props: {
-        heading: String,
-        description: String,
-        ctaText: String,
-        ctaLink: {
-            type: String,
-            default: '#'
-        }
-    }
+  props: {
+    heading: String,
+    description: String,
+    ctaText: String,
+    ctaLink: {
+      type: String,
+      default: '#',
+    },
+  },
 };
 </script>
-  
