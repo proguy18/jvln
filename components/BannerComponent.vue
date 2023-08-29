a8b48f3cddf94a4a63530bfba77ae451d3766c1e
proguy18 <josephjoey.leonardi@gmail.com>
2023-08-29 17:38:59 +0700

    feat - content component + banner component

---
+++ components/BannerComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="relative w-full overflow-hidden">
+        <a :href="bannerLink" class="block w-full">
+            <picture>
+                <source :srcset="mobileImage" media="(max-width: 440px)">
+                <source :srcset="tabletImage" media="(max-width: 768px)">
+                <source :srcset="laptopImage" media="(max-width: 1023px)">
+                <source :srcset="desktopImage" media="(max-width: 1360px)">
+                <img :src="defaultImage" alt="Banner" class="object-contain w-full">
+            </picture>
+        </a>
+    </div>
+</template>
+  
+  
+<script>
+export default {
+    props: {
+        mobileImage: String,
+        tabletImage: String,
+        laptopImage: String,
+        desktopImage: String,
+        defaultImage: String,
+        bannerLink: {
+            type: String,
+            default: '#'
+        }
+    }
+};
+</script>
+  
---
+++ components/ContentComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="p-4 text-center">
+      <h1 class="mb-4 text-xl">{{ heading }}</h1>
+      <p class="mb-4">{{ description }}</p>
+      <a :href="ctaLink" class="px-4 py-2 text-white bg-blue-500 rounded">{{ ctaText }}</a>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      heading: String,
+      description: String,
+      ctaText: String,
+      ctaLink: {
+        type: String,
+        default: '#'
+      }
+    }
+  };
+  </script>
+  
